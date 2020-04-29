7b82110cff1406b9726654b4fcedf8ca78193e21
Blade Barringer <blade.barringer@getbraintree.com>
2020-04-29 18:03:41 +0000

    Update deps

--- wdio.conf.js
+++ wdio.conf.js
@@ -117,7 +117,13 @@ exports.config = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
-  services: ['chromedriver'],
+  services: [
+    [
+      'chromedriver', {
+        args: ['--headless', '--disable-gpu']
+      }
+    ]
+  ],
 
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
