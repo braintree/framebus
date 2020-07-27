2f360660391c0d6c64a04b004321e10f63369071
Blade Barringer <blade.barringer@getbraintree.com>
2020-07-27 18:16:36 +0000

    Use @braintree/uuid for uuid generation

--- src/lib/uuid.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-mixed-operators */
-export function uuid(): string {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-    const r = (Math.random() * 16) | 0;
-    const v = c === "x" ? r : (r & 0x3) | 0x8;
-
-    return v.toString(16);
-  });
-}
