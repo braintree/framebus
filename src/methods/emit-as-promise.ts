e2442d905aecbf2a533d7a5d49a9a392e7c583d1
Blade Barringer <blade.barringer@paypal.com>
2022-01-26 14:20:00 -0600

    feat: port over emitAsPromise to functional ESM style

---
+++ src/methods/emit-as-promise.ts
@@ -0,0 +1,19 @@
+import { emit } from "./emit";
+import type { FramebusConfig } from "../framebus";
+import type { FramebusSubscriberArg } from "../lib/types";
+
+export function emitAsPromise<T = unknown>(
+  config: FramebusConfig,
+  eventName: string,
+  data?: FramebusSubscriberArg
+): Promise<T> {
+  return new Promise((resolve, reject) => {
+    const didAttachListener = emit(config, eventName, data, (payload) => {
+      resolve(payload as T);
+    });
+
+    if (!didAttachListener) {
+      reject(new Error(`Listener not added for "${eventName}"`));
+    }
+  });
+}
