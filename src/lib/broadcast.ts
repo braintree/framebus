ac0fe3c364723b2a444f79c82e009175ae955f1d
Blade Barringer <blade.barringer@paypal.com>
2022-10-28 19:41:18 +0000

    refactor: convert broadcast to take an object of options

--- src/lib/broadcast.ts
+++ src/lib/broadcast.ts
@@ -12,41 +12,40 @@ export function broadcast(payload: string, options: BroadcastOptions): void {
 
   const { origin, frames, limitBroadcastToFramesArray } = options;
 
-  const frame = frames[0];
-  // frames.forEach((frame) => {
-  try {
-    frame.postMessage(payload, origin);
+  frames.forEach((frame) => {
+    try {
+      frame.postMessage(payload, origin);
 
-    // if (limitBroadcastToFramesArray) {
-    //   return;
-    // }
+      // if (limitBroadcastToFramesArray) {
+      //   return;
+      // }
 
-    if (hasOpener(frame) && frame.opener.top !== window.top) {
-      broadcast(payload, {
-        origin,
-        frames: [frame.opener.top],
-        limitBroadcastToFramesArray,
-      });
+      if (hasOpener(frame) && frame.opener.top !== window.top) {
+        broadcast(payload, {
+          origin,
+          frames: [frame.opener.top],
+          limitBroadcastToFramesArray,
+        });
+      }
+
+      // previously, our max value was frame.frames.length
+      // but frames.length inherits from window.length
+      // which can be overwritten if a developer does
+      // `var length = value;` outside of a function
+      // scope, it'll prevent us from looping through
+      // all the frames. With this, we loop through
+      // until there are no longer any frames
+      // eslint-disable-next-line no-cond-assign
+      while ((frameToBroadcastTo = frame.frames[i])) {
+        broadcast(payload, {
+          origin,
+          frames: [frameToBroadcastTo],
+          limitBroadcastToFramesArray,
+        });
+        i++;
+      }
+    } catch (_) {
+      /* ignored */
     }
-
-    // previously, our max value was frame.frames.length
-    // but frames.length inherits from window.length
-    // which can be overwritten if a developer does
-    // `var length = value;` outside of a function
-    // scope, it'll prevent us from looping through
-    // all the frames. With this, we loop through
-    // until there are no longer any frames
-    // eslint-disable-next-line no-cond-assign
-    while ((frameToBroadcastTo = frame.frames[i])) {
-      broadcast(payload, {
-        origin,
-        frames: [frameToBroadcastTo],
-        limitBroadcastToFramesArray,
-      });
-      i++;
-    }
-  } catch (_) {
-    /* ignored */
-  }
-  // });
+  });
 }
